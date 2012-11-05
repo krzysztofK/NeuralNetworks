bf5646728de8e7a2b594b45824a9a37818eb6c65
Tomasz Wojcik <tomekwo@tlen.pl>
2012-11-06 00:42:01 +0100

    Main enhanced

--- NeuralNet/src/main/main.py
+++ NeuralNet/src/main/main.py
@@ -3,14 +3,31 @@ Created on 05-11-2012
 
 @author: Tomasz
 '''
+import sys
 from net_parser.parser import NetParser, InputVectorParser
 from net_structure.node import Node, Bias, NeuronNode
 from net_structure.layer import Layer
 from net_structure.link import Link
 from net_structure.network import NeuralNetwork
 
+ARG_COMMENT = '''Please specify arguments properly:
+python main.py network input_vector
+where
+network - xml file with neural network
+input_vector - (optional) xml file with input vector
+'''
+
 if __name__ == '__main__':
-    network = NetParser('../../resources/network_XOR.xml').parse()
-    input_vector = InputVectorParser('../../resources/inputVector.xml').parse()
+    argc = len(sys.argv)
+    if argc < 2 or argc > 3:
+        print(ARG_COMMENT)
+        sys.exit(1)
+    elif argc == 2:
+        network = NetParser(sys.argv[1]).parse()
+        input_vector = None
+    elif argc == 3:
+        network = NetParser(sys.argv[1]).parse()
+        input_vector = InputVectorParser(sys.argv[2]).parse()
+    print('Network response is:')
     print(network.calculte_answer(input_vector))
     
