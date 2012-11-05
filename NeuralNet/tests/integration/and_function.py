ca6ab22b383eba4fd95e20c363ca0fe1437b86cf
Tomasz Wojcik <tomekwo@tlen.pl>
2012-11-05 23:14:33 +0100

    Integration tests added

---
+++ NeuralNet/tests/integration/and_function.py
@@ -0,0 +1,31 @@
+'''
+Created on 05-11-2012
+
+@author: Tomasz
+'''
+import unittest
+
+
+class AndFunctionIntegrationTest(unittest.TestCase):
+
+
+    def setUp(self):
+        pass
+
+
+    def tearDown(self):
+        pass
+
+    def testWithLinearActivationFunction(self):
+        pass
+
+    def testWithSigmoidActivationFunction(self):
+        pass
+    
+    def testWithThresholdActivationFunction(self):
+        pass
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
---
+++ NeuralNet/tests/integration/xor_function.py
@@ -0,0 +1,28 @@
+'''
+Created on 05-11-2012
+
+@author: Tomasz
+'''
+import unittest
+
+
+class XorFunctionIntegrationTest(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+
+    def tearDown(self):
+        pass
+
+
+    def testWithSigmoidActivationFunction(self):
+        pass
+    
+    def testWithThresholdActivationFunction(self):
+        pass
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
