9fbe3fe86366595c0d78214201c6ef664f9dc3e7
Tomasz Wojcik <tomekwo@tlen.pl>
2012-11-26 23:59:14 +0100

    Tests added

--- NeuralNet/tests/integration/kohonen.py
+++ NeuralNet/tests/integration/kohonen.py
@@ -7,19 +7,27 @@ from net_parser.parser import InputVectorParser, NetParser
 COMMON_DIR_PREFIX = '../../resources/kohonen_networks/'
 import unittest
 
-def parseNetAndVector(net_name, vector_name):
-    return (NetParser(COMMON_DIR_PREFIX+net_name).parse(), InputVectorParser(COMMON_DIR_PREFIX+vector_name).parse())
-
 class Test(unittest.TestCase):
 
     #TODO:
     #Add more tests
 
     def testSilly(self):
-        network, vector = parseNetAndVector("kohonen_network_lab2.xml", "input1.xml")
-        for i in range(1, 10):
-            network.learn(vector, 0.5 / float(i), 0.2)
-            print(network)
+        network = NetParser(COMMON_DIR_PREFIX+"kohonen_network_lab2.xml").parse()
+        vector1 = InputVectorParser(COMMON_DIR_PREFIX+"input1.xml").parse()
+        vector2 = InputVectorParser(COMMON_DIR_PREFIX+"input2.xml").parse()
+        vector3 = InputVectorParser(COMMON_DIR_PREFIX+"input3.xml").parse()
+        vector4 = InputVectorParser(COMMON_DIR_PREFIX+"input4.xml").parse()
+        coefficient = 0.12
+        conscience_coefficient = 1.0
+        for i in range(1, 32000):
+            reducer = pow(0.5, i/8000)
+            network.learn(vector1, coefficient * reducer, conscience_coefficient * reducer)
+            network.learn(vector2, coefficient * reducer, conscience_coefficient * reducer)
+            network.learn(vector3, coefficient * reducer, conscience_coefficient * reducer)
+            network.learn(vector4, coefficient * reducer, conscience_coefficient * reducer)
+            if i % 8000 == 0:
+                print(network)
 
 
 if __name__ == "__main__":
